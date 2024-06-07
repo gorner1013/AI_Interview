8b8969244bf37204ca19320303839b3e8985b12c
gorner1013 <wimmer.fulldev@outlook.com>
2024-06-08 01:49:27 +0900

    error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,13 +71,13 @@ export default function Home() {
 	const [isInterviewEnded, setIsInterviewEnded] = useState(false);
 	const [isConnected, setIsConnected] = useState(false);
 	// マイク、ディスプレイのstream
-	const { userStream, displayStream } = useStream();
-	// 録画
-	const { startRecording, stopRecording } = useRecording(jobApplicantKey as string,interviewUuid as string　, displayStream, userStream);
-	// 音声入力
-	useSpeechInput(userStream, isMakingSpeech);
-	// 音声出力
-	const { startSpeaking } = useSpeakCharacter(isMakingSpeech);
+	// const { userStream, displayStream } = useStream();
+	// // 録画
+	// const { startRecording, stopRecording } = useRecording(jobApplicantKey as string,interviewUuid as string　, displayStream, userStream);
+	// // 音声入力
+	// useSpeechInput(userStream, isMakingSpeech);
+	// // 音声出力
+	// const { startSpeaking } = useSpeakCharacter(isMakingSpeech);
 	// インタビュー
 	// useInterview(isMakingSpeech, startSpeaking, startRecording, stopRecording, () => {
 	// 	setIsInterviewEnded(true);
