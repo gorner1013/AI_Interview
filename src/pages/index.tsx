db4711cd0dd107abfc9e69931af66bfb8a1f7b1c
gorner1013 <wimmer.fulldev@outlook.com>
2024-06-08 03:35:13 +0900

    error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,7 +77,7 @@ export default function Home() {
 	// 音声入力
 	useSpeechInput(userStream, isMakingSpeech);
 	// 音声出力
-	// const { startSpeaking } = useSpeakCharacter(isMakingSpeech);
+	const { startSpeaking } = useSpeakCharacter(isMakingSpeech);
 	// // インタビュー
 	// useInterview(isMakingSpeech, startSpeaking, startRecording, stopRecording, () => {
 	// 	setIsInterviewEnded(true);
@@ -114,7 +114,7 @@ export default function Home() {
 	const { connect, disconnect } = useWebsocket();
 
 	useEffect(() => {
-		console.log("11useEffect - isInterviewStarted:", isInterviewStarted);
+		console.log("1useEffect - isInterviewStarted:", isInterviewStarted);
 		console.log("useEffect - isConnected:", isConnected);
 		console.log("useEffect - webSocketUrl:", webSocketUrl);
 
