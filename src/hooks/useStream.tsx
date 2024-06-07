c4285126eec783b21af764f1e3f0f447cc1bf67f
gorner1013 <wimmer.fulldev@outlook.com>
2024-06-08 03:18:36 +0900

    error

--- src/hooks/useStream.tsx
+++ src/hooks/useStream.tsx
@@ -25,17 +25,18 @@ export const useStream = () => {
 	const getStream = () => {
 		console.debug(`${LOG_PREFIX} Start stream acquisition process.`);
 		Promise.all([
-			navigator.mediaDevices.getUserMedia({audio: true,video: { width: 427,height: 240 } }),
+			navigator.mediaDevices.getUserMedia({audio: true }),
+			navigator.mediaDevices.getUserMedia({video: { width: 427,height: 240 }, audio: true }),
 			// navigator.mediaDevices.getDisplayMedia({ audio: true }),
 		])
 			.then((results) => {
 				// ディスプレイのシステム音声が許可されているか確認
 				if (results[0].getAudioTracks().length < 1) throw new Error('Display audio is not allowed.');
 				setUserStream(results[0]);
-				setDisplayStream(results[0]);
+				setDisplayStream(results[1]);
 				// setDisplayStream(results[1]);
 				console.debug(`${LOG_PREFIX} Stream acquisition process completed.`);
-				console.log("results[0]",results[0]);
+				console.log("results[0]00",results[0]);
 			})
 			.catch((err) => {
 				console.debug(`${LOG_PREFIX} Could not get stream.`);
